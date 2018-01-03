705878fed4a03a8f6b389bbcef39724527e85314
ulternate <contact@danielcswain.com>
2018-01-03 16:22:59 +1100

    Add columns to the database.
    This resolves issues #15 and #20 by:
    - Adding `originalDescription` and `paymentApp` columns to the database.
    - Removing the default category, marking unknown transactions as unknown.

--- app/src/main/java/com/ulternate/paycat/data/AppDatabase.java
+++ app/src/main/java/com/ulternate/paycat/data/AppDatabase.java
@@ -1,10 +1,13 @@
 package com.ulternate.paycat.data;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.arch.persistence.room.TypeConverters;
+import android.arch.persistence.room.migration.Migration;
 import android.content.Context;
+import android.support.annotation.NonNull;
 
 /**
  * Data holder for the application.
@@ -12,7 +15,7 @@ import android.content.Context;
  * Extends RoomDatabase and serves as the access point for the application data.
  */
 
-@Database(entities = {Transaction.class}, version = 1)
+@Database(entities = {Transaction.class}, version = 2)
 @TypeConverters({Converters.class})
 public abstract class AppDatabase extends RoomDatabase{
 
@@ -20,6 +23,23 @@ public abstract class AppDatabase extends RoomDatabase{
     private static AppDatabase instance;
 
     public abstract TransactionDao transactionDao();
+
+    /**
+     * Migration from version 1 to version 2.
+     * Adds two columns, "originalDescription" and "paymentApp"
+     */
+    private static final Migration MIGRATION1_2 = new Migration(1, 2) {
+
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            // Add the new columns to the database.
+            database.execSQL("ALTER TABLE transactions "
+            + " ADD COLUMN originalDescription TEXT");
+
+            database.execSQL("ALTER TABLE transactions "
+            + " ADD COLUMN paymentApp TEXT");
+        }
+    };
 
     /**
      * Get the AppDatabase as a Singleton.
@@ -32,6 +52,7 @@ public abstract class AppDatabase extends RoomDatabase{
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     AppDatabase.class,
                     DATABASE_NAME)
+                    .addMigrations(MIGRATION1_2)
                     .build();
         }
 
--- app/src/main/java/com/ulternate/paycat/data/Transaction.java
+++ app/src/main/java/com/ulternate/paycat/data/Transaction.java
@@ -24,24 +24,33 @@ public class Transaction implements Serializable{
 
     public String description;
 
+    public String originalDescription;
+
     public String category;
+
+    public String paymentApp;
 
     @TypeConverters(Converters.class)
     public Date date;
 
     /**
      * Public constructor for a Transaction.
-     *
-     * @param amount: float, the amount for the transaction.
-     * @param description: String, description for the transaction.
-     * @param category: String, category for the transaction.
-     * @param date: Date, date (incl. time) for the transaction.
+     * @param amount : float, the amount for the transaction.
+     * @param description : String, description for the transaction, this is the description field
+     *                    edited by the User.
+     * @param originalDescription : String, the original description for the transaction (i.e. from
+ *                            the merchant).
+     * @param category : String, category for the transaction.
+     * @param paymentApp : String, the application which sent the original notification.
+     * @param date : Date, date (incl. time) for the transaction.
      */
-    public Transaction(float amount, String description, String category,
-                       Date date) {
+    public Transaction(float amount, String description, String originalDescription, String category,
+                       String paymentApp, Date date) {
         this.amount = amount;
         this.description = description;
+        this.originalDescription = originalDescription;
         this.category = category;
+        this.paymentApp = paymentApp;
         this.date = date;
     }
 }
